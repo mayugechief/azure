a6d8971beeb3cdb9b8f8eeb93e9ccb03d5000dea
timanaka <timanaka@engineyard.com>
2013-09-07 14:14:00 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Azure::Application.config.secret_token = 'ffa452056af28c5ca5c3e6378b2c7cd6d48578b57e044f79685cff6e61aa724a89f8a379423c8546aafdca717dc9c1855f74f831fe62bcb915c5dcd09ffcd89d'
